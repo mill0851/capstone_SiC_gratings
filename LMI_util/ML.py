2736623ef7ea3ea5fbf28528fc850d4c5f5f9bf4
Robert_Miller <robert.a.miller.410@gmail.com>
2026-02-11 10:48:05 -0600

    added a robust function for importing comsol data

--- LMI_util/ML.py
+++ LMI_util/ML.py
@@ -1,8 +1,10 @@
 import pandas as pd
-import torch
-from torch.utils.data import Dataset
+import os
+from typing import List
+import numpy as np
+from ComsolDataset import ComsolDataset
 
-def import_comsol_data(root_dir: str, meta_data_filename: str):
+def import_comsol_data(root_dir: str, meta_data_filename: str, data_filenames: List[str], data_column_values: dict) -> ComsolDataset:
     """
     This function is used to import comsol simulation data. It works with
     the specific directory structure:
@@ -24,8 +26,58 @@ def import_comsol_data(root_dir: str, meta_data_filename: str):
     :type root_dir: str
 
     """
-    return 0
 
-class SpectraDataset(Dataset):
- def __init__():
-    return 0
+    # Read in metadata
+    meta_path = os.path.join(root_dir, meta_data_filename)
+    meta_table = pd.read_csv(meta_path)
+    meta_table["run_id"] = meta_table["run_id"].astype(str)
+    meta_table = meta_table.set_index("run_id").sort_index()
+    print(f"meta table: {meta_table}")
+
+    # Read in target data
+    data_dfs = []
+    runs_path = os.path.join(root_dir, "runs")
+    print(f"runs path: {runs_path}")
+
+    for filename in data_filenames:
+        data_list = []
+
+        for item in os.listdir(runs_path):
+            item_path = os.path.join(runs_path, item)
+
+            if os.path.isdir(item_path):
+                data_path = os.path.join(item_path, filename)
+                print(f"data path: {data_path}")
+                data_table = pd.read_csv(data_path)
+                data_table["run_id"] = data_table["run_id"].astype(str)
+                data_list.append(data_table)
+
+        data_df = pd.concat(data_list, ignore_index=True)
+        print(f"dataframe: {data_df}")
+        data_dfs.append(data_df)
+
+    # pivot data to one run per row
+    for i, key in enumerate(data_column_values):
+       data_pivot = data_dfs[i].pivot(index="run_id", columns=key, values=data_column_values[key])
+       data_pivot = data_pivot.sort_index()
+       data_dfs[i] = data_pivot
+       print(f"pivot df: {data_pivot}")
+
+    # Validate that the meta data and data is aligned
+    for data in data_dfs:
+       assert (data.index == meta_table.index).all()
+
+    # Convert to numpy
+    X_meta = meta_table.to_numpy(dtype=np.float32)
+    X_data = []
+    for data in data_dfs:
+       X_data.append(data.to_numpy(dtype=np.float32))
+    
+    # Create dataset object
+    data_final = ComsolDataset(X_meta, X_data, data_filenames)
+    return data_final
+
+if __name__ == "__main__":
+    root_dir = r"C:\Users\robert\Code\capstone\capstone_SiC_gratings\data\ML\test_batch"
+    data = import_comsol_data(root_dir, "geometries.csv", ["reflectance.csv"], {"wavelength_um":"reflectance_0"})
+    
